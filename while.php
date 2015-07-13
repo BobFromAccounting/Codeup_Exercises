d2411b18f54b23e40345b5c7fa548163200a98ee
Tarek S Hafez <thafez@ymail.com>
2015-07-13 11:17:37 -0500

    Modified while.php

--- while.php
+++ while.php
@@ -1,9 +1,19 @@
 <?php
 
 $test = 5;
+$firstCondition = 3;
+$secondCondition = 2;
 
 while ($test <= 15) {
-	echo "$test" . PHP_EOL;
+	if ($test % $firstCondition == 0 && $test % $secondCondition == 0) {
+		echo "{$test} is divisible by both {$firstCondition} and {$secondCondition}." . PHP_EOL;
+	} else if ($test % $firstCondition == 0) {
+		echo "{$test} is divisible by {$firstCondition}." . PHP_EOL;
+	} else if ($test % $secondCondition == 0) {
+		echo "{$test} is divisible by {$secondCondition}." . PHP_EOL;
+	} else {
+		echo "{$test}" . PHP_EOL;
+	}
 	$test += 1;
 }
 
