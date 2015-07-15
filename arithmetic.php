0ce83e95106c880a482e33d5ae885d14921a6c45
Tarek S Hafez <thafez@ymail.com>
2015-07-15 11:34:38 -0500

    Final modification for arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,35 @@
 <?php
 
+$a = 100;
+$b = 50;
+
 function add($a, $b)
 {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b)
 {
-    echo $a - $b;
+    echo $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b)
 {
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b)
 {
-    echo $a / $b;
+    echo $a / $b . PHP_EOL;
 }
 
-$a = 100;
-$b = 50;
+function modulus($a, $b)
+{
+	echo $a % $b . PHP_EOL;
+}
 
 echo add($a, $b);
 echo subtract($a, $b);
 echo multiply($a, $b);
 echo divide($a, $b);
+echo modulus($a, $b);
