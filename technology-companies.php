dfbcdf8661113a1923fadecd1843c9011ef07bc5
Tarek S Hafez <thafez@ymail.com>
2015-07-16 10:38:21 -0500

    modified to print out all details of array.

--- technology-companies.php
+++ technology-companies.php
@@ -1,55 +1,55 @@
 <?php
 
-$companies = [
-    'Sun Microsystems' => [
-        'Vinod Khosla',
-        'Bill Joy',
-        'Andy Bechtolsheim',
-        'Scott McNealy'
-    ],
-    'Silicon Graphics' => [
-        'Jim Clark',
-        'Ed McCracken'
-    ],
-    'Cray' => [
-        'William Norris',
-        'Seymour Cray'
-    ],
-    'NeXT' => [
-        'Steve Jobs',
-        'Avie Tevanian',
-        'Joanna Hoffman',
-        'Bud Tribble',
-        'Susan Kare'
-    ],
-    'Acorn Computers' => [
-        'Steve Furber',
-        'Sophie Wilson',
-        'Hermann Hauser',
-        'Jim Mitchell'
-    ],
-    'MIPS Technologies' => [
-        'Skip Stritter',
-        'John L. Hennessy'
-    ],
-    'Commodore' => [
-        'Yash Terakura',
-        'Bob Russell',
-        'Bob Yannes',
-        'David A. Ziembicki',
-        'Jay Miner'
-    ],
-    'Be Inc' => [
-        'Steve Sakoman',
-        'Jean-Louis Gassée'
-    ]
-];
+    $companies = [
+        'Sun Microsystems' => [
+            'Vinod Khosla',
+            'Bill Joy',
+            'Andy Bechtolsheim',
+            'Scott McNealy'
+        ],
+        'Silicon Graphics' => [
+            'Jim Clark',
+            'Ed McCracken'
+        ],
+        'Cray' => [
+            'William Norris',
+            'Seymour Cray'
+        ],
+        'NeXT' => [
+            'Steve Jobs',
+            'Avie Tevanian',
+            'Joanna Hoffman',
+            'Bud Tribble',
+            'Susan Kare'
+        ],
+        'Acorn Computers' => [
+            'Steve Furber',
+            'Sophie Wilson',
+            'Hermann Hauser',
+            'Jim Mitchell'
+        ],
+        'MIPS Technologies' => [
+            'Skip Stritter',
+            'John L. Hennessy'
+        ],
+        'Commodore' => [
+            'Yash Terakura',
+            'Bob Russell',
+            'Bob Yannes',
+            'David A. Ziembicki',
+            'Jay Miner'
+        ],
+        'Be Inc' => [
+            'Steve Sakoman',
+            'Jean-Louis Gassée'
+        ]
+    ];
 
-    arsort($companies);
+    print_r($companies);
+//     arsort($companies);
 
-foreach ($companies as $key => $people) {
-    echo "$key" . PHP_EOL;
-    sort($people);
-    print_r($people) . PHP_EOL;
-}
-    // print_r($companies) . PHP_EOL;
+//     foreach ($companies as $companyName => $people) {
+//         echo "$companyName" . PHP_EOL;
+//         sort($people);
+//         print_r($people) . PHP_EOL;
+// }
