a8c2f6a22672ee5a9ff54b649bc550d743659555
Tarek S Hafez <thafez@ymail.com>
2015-07-17 14:48:49 -0500

    Modified contact-parser.php to add options for completion

--- contact-parser.php
+++ contact-parser.php
@@ -1,12 +1,41 @@
 <?php
 
-function parseContacts($filename)
-{
-    $contacts = array();
+$filename = 'contacts.txt';
 
-    // todo - read file and parse contacts
+function parseContacts($filename) {
+	$contacts = array();
 
+	$handle = fopen($filename, 'r');
+	$contents = trim(fread($handle, filesize($filename)));
+	$entries = explode(PHP_EOL, $contents);
+	
+	foreach ($entries as $key => $entry) {
+
+		$innerArray = explode('|', $entry);
+
+		$contacts[$key]['name'] = $innerArray[0];
+		$contacts[$key]['number'] = formatNumbers($innerArray[1]);
+
+		// option 2
+		// $contacts[] = array(
+		// 	'name' => $innerArray[0],
+		// 	'number' => formatNumbers($innerArray[1])
+		// );
+		
+		// option 3
+		// $innerArray['name'] = explode('|', $entry)[0];
+		// $innerArray['number'] = explode('|', $entry)[1];
+		// $innerArray['number'] = formatNumbers($innerArray['number']);
+		// array_push($contacts, $innerArray);
+	}
+	
+	fclose($handle);
     return $contacts;
 }
 
-var_dump(parseContacts('contacts.txt'));
+function formatNumbers ($number) {
+	$number = '(' . substr($number, 0, 3) . ') ' . substr($number, 3, 3) . '-' . substr($number, 6, 4);
+	return $number;
+}
+
+var_dump(parseContacts($filename));
