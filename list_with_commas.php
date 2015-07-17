2ee7cbd1c74ac39088ba212b05d7c9f002a54c0b
Tarek S Hafez <thafez@ymail.com>
2015-07-17 10:11:31 -0500

    Adding list_with_commas.php

---
+++ list_with_commas.php
@@ -0,0 +1,20 @@
+<?php
+
+	function humanizedList($array, $sort = false) {
+		if ($sort) {
+			sort($array);
+		}
+		$lastInArray = array_pop($array);
+		$modifiedLastInArray = array_push($array, "and {$lastInArray}");
+		return implode(", ", $array);
+	}
+
+	$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+	$stringToArray = explode(', ', $physicistsString);
+
+	$famousFakePhysicists = humanizedList($stringToArray, true);
+
+	echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
+
+?>
