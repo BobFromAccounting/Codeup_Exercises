52658dd2cfd6a08185153fdd40f119717289e97d
Tarek S Hafez <thafez@ymail.com>
2015-07-19 18:35:11 -0500

    Modified blackjack.php and euler.php

--- euler.php
+++ euler.php
@@ -1,12 +1,18 @@
 <?php
-	$sum = 0;
 
-	for ($i = 0; $i < 1000; $i++) {
-		if ($i % 3 == 0 || $i % 5 == 0) {
-			$sum += $i;
+	// project euler 1
+	function problemOne () {
+		$sum = 0;
+
+		for ($i = 0; $i < 1000; $i += 1) {
+			if ($i % 3 == 0 || $i % 5 == 0) {
+				$sum += $i;
+			}
 		}
+
+		echo("$sum" . PHP_EOL);
 	}
 
-	echo("$sum" . PHP_EOL);
+	problemOne();
 
 ?>
